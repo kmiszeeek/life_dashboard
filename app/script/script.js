0a1adbc1333d531973f819cd9e561ea25bc95072
wiktorkmiecik2p <73634322+wiktorkmiecik2p@users.noreply.github.com>
2022-10-03 00:28:47 +0200

    added basic comunication with weather api

--- app/script/script.js
+++ app/script/script.js
@@ -4,19 +4,47 @@ function getLocation() {
   }
 }
 
-getLocation();
+async function loadNames() {
+  const response = await fetch("./resources/countries.json");
+  const names = await response.json();
+  console.log(names[176].Name);
+  return names;
+}
+// loadNames();
 
-function getWeather(position) {
-  //   console.log(position.coords.latitude);
-  //   console.log(position.coords.longitude);
-  fetch(
-    `api.openweathermap.org/data/2.5/forecast?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=56e1b69633d0faa92c0b2d5121e0c2b1`
-  )
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+// function convertFromCountryShortcut(shortcut) {
+//   // let countriesData = JSON.parse(countries);
+//   let countries = loadNames();
+//   console.log(shortcut + countriesData[176].name);
+// }
+
+function convertFromTimestamp(timestamp) {
+  let tmp = new Date(timestamp * 1000);
+  let hours = tmp.getHours();
+  let minutes = "0" + tmp.getMinutes();
+  let seconds = "0" + tmp.getSeconds();
+  let result = hours + ":" + minutes.substr(-2) + ":" + seconds.substr(-2);
+
+  return result;
 }
 
-// api.openweathermap.org/data/2.5/forecast?lat=1&lon=2&appid=56e1b69633d0faa92c0b2d5121e0c2b1
-// fetch("https://random-data-api.com/api/v2/users")
-//   .then((response) => response.json())
-//   .then((data) => console.log(data));
+getLocation();
+
+let weatherData;
+
+function getWeather(position) {
+  console.log(position.coords.latitude);
+  console.log(position.coords.longitude);
+  fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=56e1b69633d0faa92c0b2d5121e0c2b1&units=metric&mode=json`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.city.name + ", " + data.city.country);
+      // console.log(convertFromCountryShortcut(data.city.country));
+      console.log(data.city.country);
+      console.log(convertFromTimestamp(data.city.sunrise));
+      console.log(convertFromTimestamp(data.city.sunset));
+      console.log(data.list[0].main.temp + "°C");
+    });
+}
